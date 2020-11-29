c28d5cbc11913cc9c5295da05fee002fcf37f890
AlexandreNogueiraMendonca <alexandre.nogueira-mendonca@etu.hesge.ch>
2020-11-29 20:43:07 +0100

    update

--- src/components/FormUpdateProduct.vue
+++ src/components/FormUpdateProduct.vue
@@ -2,11 +2,11 @@
   <div>
     <h2>Update a product</h2>
     <div>
-      <input :value="myProduct.name"  name="name" label="Name" required @input="changeProduct(myProduct, 1)">
-      <input :value="myProduct.price" type="number" name="price" label="Price" required @input="changeProduct(myProduct, 2)"/>
-      <input :value="myProduct.stock" type="number" name="stock" label="Stock" required @input="changeProduct(myProduct, 3)"/>
+      <input :value="myProduct.name"  name="name"  required @input="changeProduct(myProduct, 1)">
+      <input :value="myProduct.price" type="number" name="price"  required @input="changeProduct(myProduct, 2)"/>
+      <input :value="myProduct.stock" type="number" name="stock" required @input="changeProduct(myProduct, 3)"/>
       <input :value="myProduct.minimum_stock" type="number" name="minimum_stock" label="Minimum Stock" required @input="changeProduct(myProduct, 4)"/>
-      <button type="submit" name="updateProduct" label="Add" @click="updateProduct">Update</button>
+      <button type="submit" name="updateProduct" @click="updateProduct">Update</button>
     </div>
   </div>
 </template>
